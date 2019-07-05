f483b63b76fd03750b58ab53509c6403a33ce727
akrommusajid <a.musajid@gmail.com>
2019-07-05 15:00:35 +0700

    fix deploy

--- complex/deploy.sh
+++ complex/deploy.sh
@@ -1,6 +1,6 @@
-docker build -t akrommusajid/multi-client:latest -t akrommusajid/multi-client:$SHA -f ./client/Dockerfile ./client
-docker build -t akrommusajid/multi-server:latest -t akrommusajid/multi-server:$SHA -f ./server/Dockerfile ./server
-docker build -t akrommusajid/multi-worker:latest -t akrommusajid/multi-worker:$SHA -f ./worker/Dockerfile ./worker
+docker build -t akrommusajid/multi-client:latest -t akrommusajid/multi-client:$SHA -f ./client/Dockerfile ./complex/client
+docker build -t akrommusajid/multi-server:latest -t akrommusajid/multi-server:$SHA -f ./server/Dockerfile ./complex/server
+docker build -t akrommusajid/multi-worker:latest -t akrommusajid/multi-worker:$SHA -f ./worker/Dockerfile ./complex/worker
 
 docker push akrommusajid/multi-client:latest
 docker push akrommusajid/multi-server:latest
@@ -9,7 +9,7 @@ docker push akrommusajid/multi-client:$SHA
 docker push akrommusajid/multi-server:$SHA
 docker push akrommusajid/multi-worker:$SHA
 
-kubectl apply -f k8s
+kubectl apply -f ./complex/k8s
 kubectl set image deployments/server-deployment server=akrommusajid/multi-server:$SHA
 kubectl set image deployments/client-deployment client=akrommusajid/multi-client:$SHA
 kubectl set image deployments/worker-deployment worker=akrommusajid/multi-worker:$SHA
